08486643b1149c2253259344cf78ab3eb46e725c
Radoslav <rudix.lab@gmail.com>
2020-08-20 21:57:47 +0300

    c

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/arpecop/Desktop/aws-amplify-gatsby-auth/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/arpecop/Desktop/aws-amplify-gatsby-auth/src/pages/404.js"))),
   "component---src-pages-app-js": hot(preferDefault(require("/Users/arpecop/Desktop/aws-amplify-gatsby-auth/src/pages/app.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/arpecop/Desktop/aws-amplify-gatsby-auth/src/pages/index.js")))
