e44f761113df31a1710d8d7b2d36bc492215bf07
arpecop <arpecop@users.noreply.github.com>
2020-09-28 14:14:37 +0000

    deploy: 5cbc2f23d4124ab1a265207759a90d51b118baed

---
+++ precache-manifest.ef1e5e14a03fb5a49f20115d75f3265a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6693bdd8224e1607c10e012e1f1ad11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c77242e490b4ecc5f",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "0ba31962f89b1be2a3c3",
+    "url": "/static/css/main.9a8fe7f3.chunk.css"
+  },
+  {
+    "revision": "165c77242e490b4ecc5f",
+    "url": "/static/js/2.85e1640b.chunk.js"
+  },
+  {
+    "revision": "d40dc0569182069f55090832a7fb87c4",
+    "url": "/static/js/2.85e1640b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba31962f89b1be2a3c3",
+    "url": "/static/js/main.4f46a8a1.chunk.js"
+  },
+  {
+    "revision": "363f11a49c64d028fc57",
+    "url": "/static/js/runtime-main.803a66c0.js"
+  }
+]);
