662cd46e2428161ffa37e8fd0bd5c57f2933844b
Radoslav <rudix.lab@gmail.com>
2020-09-29 17:09:38 +0300

    c

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import { loggedInUserData } from './utils/state';
 Amplify.configure({
   aws_project_region: 'eu-west-1',
   aws_cognito_identity_pool_id:
-      'eu-west-1:06863165-5b56-4598-8e87-cc9ae2895f39',
+    'eu-west-1:06863165-5b56-4598-8e87-cc9ae2895f39',
   aws_cognito_region: 'eu-west-1',
   aws_user_pools_id: 'eu-west-1_sk5LfmU4g',
   aws_user_pools_web_client_id: '6d5of62ku9g4pcl2jpp0rcng67',
--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -1,113 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Error from '../Error';
 import { Auth } from 'aws-amplify';
 import { Input } from 'antd';
+import Error from '../Error';
 import Login from './Login';
 
-class SignUp extends React.Component {
-  state = {
-    username: ``,
-    password: ``,
+const SignUp = () => {
+  const [state, setState] = useState({
+    username: '',
+    password: '',
     email: '',
     authCode: '',
     stage: 0,
     error: '',
-  };
-
-  handleUpdate = (event) => {
-    this.setState({
+  });
+  const handleUpdate = (event) => {
+    setState({
+      ...state,
       [event.target.name]: event.target.value,
     });
   };
 
-  signUp = async () => {
-    const { username, password, email } = this.state;
+  const signUp = async () => {
+    const { username, password, email } = state;
     try {
       await Auth.signUp({
         username,
         password,
         attributes: { email },
       });
-      this.setState({ stage: 1 });
+      setState({ ...state, stage: 1 });
     } catch (err) {
-      this.setState({ error: err });
+      setState({ ...state, error: err });
       console.log('error signing up...', err);
     }
   };
 
-  confirmSignUp = async () => {
-    const { username, authCode } = this.state;
+  const confirmSignUp = async () => {
+    const { username, authCode } = state;
     try {
       await Auth.confirmSignUp(username, authCode);
-      this.setState({ stage: 2 });
+      setState({ ...state, stage: 2 });
     } catch (err) {
-      this.setState({ error: err });
+      setState({ ...state, error: err });
       console.log('error confirming signing up...', err);
     }
   };
+  return (
+    <div>
+      {state.stage === 0 && (
+        <div style={styles.formContainer}>
+          <h1>Регистрация</h1>
+          {state.error && <Error errorMessage={state.error} />}
+          <Input
+            onChange={handleUpdate}
+            placeholder="Потребител"
+            name="username"
+            value={state.username}
+            style={styles.Input}
+          />
+          <Input
+            onChange={handleUpdate}
+            placeholder="Парола"
+            name="password"
+            value={state.password}
+            type="password"
+            style={styles.Input}
+          />
+          <Input
+            onChange={handleUpdate}
+            placeholder="Email"
+            name="email"
+            value={state.email}
+            style={styles.Input}
+          />
 
-  render() {
-    return (
-      <div>
-        {this.state.stage === 0 && (
-          <div style={styles.formContainer}>
-            <h1>Регистрация</h1>
-            {this.state.error && <Error errorMessage={this.state.error} />}
-            <Input
-              onChange={this.handleUpdate}
-              placeholder="Потребител"
-              name="username"
-              value={this.state.username}
-              style={styles.Input}
-            />
-            <Input
-              onChange={this.handleUpdate}
-              placeholder="Парола"
-              name="password"
-              value={this.state.password}
-              type="password"
-              style={styles.Input}
-            />
-            <Input
-              onChange={this.handleUpdate}
-              placeholder="Email"
-              name="email"
-              value={this.state.email}
-              style={styles.Input}
-            />
-
-            <div style={styles.button} onClick={this.signUp}>
-              <span style={styles.buttonText}>Sign Up</span>
-            </div>
+          <div style={styles.button} onClick={signUp}>
+            <span style={styles.buttonText}>Sign Up</span>
           </div>
-        )}
-        {this.state.stage === 1 && (
-          <div style={styles.formContainer}>
-            <h1>Регистрация</h1>
-            {this.state.error && <Error errorMessage={this.state.error} />}
-            <Input
-              onChange={this.handleUpdate}
-              placeholder="Authorization Code"
-              name="authCode"
-              value={this.state.authCode}
-              style={styles.Input}
-            />
-            <div style={styles.button} onClick={this.confirmSignUp}>
-              <span style={styles.buttonText}>Confirm Sign Up</span>
-            </div>
+        </div>
+      )}
+      {state.stage === 1 && (
+        <div style={styles.formContainer}>
+          <h1>Регистрация</h1>
+          {state.error && <Error errorMessage={state.error} />}
+          <Input
+            onChange={handleUpdate}
+            placeholder="Authorization Code"
+            name="authCode"
+            value={state.authCode}
+            style={styles.Input}
+          />
+          <div style={styles.button} onClick={confirmSignUp}>
+            <span style={styles.buttonText}>Confirm Sign Up</span>
           </div>
-        )}
+        </div>
+      )}
 
-        {this.state.stage === 2 && (
-          <>
-            Регистрациятя успешна , <a href="/app/login">Вход</a>
-          </>
-        )}
-      </div>
-    );
-  }
-}
+      {state.stage === 2 && (
+        <>
+          Регистрациятя успешна
+          <a href="/app/login">Вход</a>
+          <Login />
+        </>
+      )}
+    </div>
+  );
+};
 
 const styles = {
   Input: {
