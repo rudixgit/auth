89cb0bd4390d78911b192feeb4c467d0434c1a40
Radoslav <rudix.lab@gmail.com>
2020-09-30 15:48:41 +0300

    c

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const env1 = {
   development: {
-    api: 'https://rudixlab.com/db/',
+    api: 'https://47ruk9bopl.execute-api.eu-central-1.amazonaws.com/dev/',
   },
   production: {
-    api: 'https://rudixlab.com/db/',
+    api: 'https://47ruk9bopl.execute-api.eu-central-1.amazonaws.com/dev/',
   },
 };
 export const env = env1[process.env.NODE_ENV];
