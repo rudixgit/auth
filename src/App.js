59f270783f646014e4199aee5c866a90c5bc05c8
Radoslav <rudix.lab@gmail.com>
2020-10-01 09:06:40 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ import { loggedInUserData } from './utils/state';
 Amplify.configure({
   aws_project_region: 'eu-west-1',
   aws_cognito_identity_pool_id:
-    'eu-west-1:06863165-5b56-4598-8e87-cc9ae2895f39',
+    'eu-west-1:3f5f916d-2252-487f-99c0-7aa69115f973',
   aws_cognito_region: 'eu-west-1',
-  aws_user_pools_id: 'eu-west-1_sk5LfmU4g',
-  aws_user_pools_web_client_id: '6d5of62ku9g4pcl2jpp0rcng67',
+  aws_user_pools_id: 'eu-west-1_T6v05tjzh',
+  aws_user_pools_web_client_id: 'eqlretnsetkj5p57bqtandjqa',
   oauth: {},
 });
 
