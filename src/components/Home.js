2e11557e9f222fc02c10c8b175749fc70e495cf5
Radoslav <rudix.lab@gmail.com>
2020-10-03 12:02:32 +0300

    c

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useForm, Controller } from 'react-hook-form';
 import { post } from '../utils/api';
 
 const Home = ({ user }) => {
   const [data, setData] = useState({ Items: [] });
+  const {
+    control, register, errors, handleSubmit,
+  } = useForm();
 
+  const onSubmit = (data) => console.log(data);
   useEffect(() => {
     async function fetchData() {
       const response = await post(
@@ -23,6 +29,24 @@ const Home = ({ user }) => {
     <div>
       <h1>Начало</h1>
       <h1>{user.sub}</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          as={(
+            <TextField
+              id="outlined-basic"
+              error={!!errors.task}
+              label={errors.task ? 'task is required' : 'task'}
+              variant="outlined"
+              style={{ width: '100%' }}
+            />
+          )}
+          name="task"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+        />
+        <input type="submit" />
+      </form>
       <hr />
       {JSON.stringify(data)}
     </div>
