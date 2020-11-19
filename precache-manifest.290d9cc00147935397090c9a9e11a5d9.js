23fef038bf18570b4cb94489c346e7cf45d25519
arpecop <arpecop@users.noreply.github.com>
2020-11-19 12:45:24 +0000

    deploy: 91629ff181198d963c552f47b783aec7b8c24c62

--- precache-manifest.290d9cc00147935397090c9a9e11a5d9.js
+++ precache-manifest.290d9cc00147935397090c9a9e11a5d9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cb74413ae77167485319985b43554514",
+    "revision": "5c525919f016deb977b1e0409900cdc6",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.405d60bc.chunk.css"
   },
   {
-    "revision": "c185242cd88c8244de08",
+    "revision": "3ec3128b4cb200212246",
     "url": "/static/css/main.11eb4e7a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.7fd08c35.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c185242cd88c8244de08",
-    "url": "/static/js/main.adb32d52.chunk.js"
+    "revision": "3ec3128b4cb200212246",
+    "url": "/static/js/main.b47fbfa8.chunk.js"
   },
   {
     "revision": "363f11a49c64d028fc57",
