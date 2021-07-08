bbf07a1a073e4f526da97ca2d982ba726568f960
arpecop <arpecop@users.noreply.github.com>
2021-07-08 17:03:27 +0000

    deploy: c92b4f1394d0fb110d410dff3d6ec412d78059e4

---
+++ precache-manifest.219e9a9131c07e6a3dc40afa3bba98f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec06151b8ad381ca5151560e4b1be41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37077441c7aec45b9f14",
+    "url": "/static/css/2.3c09dbcf.chunk.css"
+  },
+  {
+    "revision": "cf67efd21f46025ca79f",
+    "url": "/static/css/main.924f4a0a.chunk.css"
+  },
+  {
+    "revision": "37077441c7aec45b9f14",
+    "url": "/static/js/2.2524c548.chunk.js"
+  },
+  {
+    "revision": "779f43fa5d28e2794421c0357fcacb1c",
+    "url": "/static/js/2.2524c548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf67efd21f46025ca79f",
+    "url": "/static/js/main.1f1ed246.chunk.js"
+  },
+  {
+    "revision": "363f11a49c64d028fc57",
+    "url": "/static/js/runtime-main.803a66c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38125ca56f9af8383844742aa2390fb9.js"
+  "/precache-manifest.219e9a9131c07e6a3dc40afa3bba98f1.js"
 );
 
 self.addEventListener('message', (event) => {
